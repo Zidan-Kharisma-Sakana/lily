b9d16b996cd4d635137d6d75599cc861ddac167e
Zidan Kharisma <zidan.kharisma@ui.ac.id>
2022-06-26 19:20:02 +0700

    revisi 7

--- components/Dashboard/Card/RegisteredCard.tsx
+++ components/Dashboard/Card/RegisteredCard.tsx
@@ -9,7 +9,7 @@ export const RegisteredCard: FC<RegisteredEventsData> = ({ event, date }) => {
   return (
     <div className="glassCard flex justify-between items-center w-full mb-3">
       <p>{event}</p>
-      <p className="text-xs font-bold">2 - 3 July</p>
+      <p className="text-xs font-bold">2 - 15 July</p>
     </div>
   );
 };
